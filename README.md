2b78437b502711d29ec70b0c7e3e9808149add34
Mark Simpson <mark.simpson@asset.tv>
2018-03-28 16:03:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# LEMPSTACK
+quick and easy LEMP stack with maria/php 7.1
+
+## Installation
+
+Run docker-compose up
+
+Place your code in ./html directory
+
+View on localhost:8000
---
+++ html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Marks Base</title>
+</head>
+<body>
+    <h1>Hello NGINX</h1>
+    <p><?= "Hello" ?></p>
+    <?php phpinfo(); ?>
+</body>
+</html>
